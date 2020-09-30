f1011291f6cdb984fdacadf7b16dc8f75dd9a2ca
Lillian Parker <Lillianpparker@outlook.com>
2020-09-30 12:46:25 -0500

    updates

--- fauna-favor-client/src/components/AnimalList/AnimalList.js
+++ fauna-favor-client/src/components/AnimalList/AnimalList.js
@@ -7,8 +7,15 @@ export default function AnimalList(props) {
             {props.animals.map(animal => {
                 console.log(animal)
                 return (
-                    <div>
+                    <div className="showAnimal">
                         <h1>{animal.primaryCommonName}</h1>
+                        <h1>{animal.scientificName}</h1>
+                        <h2>{animal.iucn.iucnDescEn}</h2>
+                        <h3>{animal.speciesGlobal.taxorder}</h3>
+                        <h3>{animal.speciesGlobal.family}</h3>
+                        <h3>{animal.speciesGlobal.genus}</h3>
+                        <p>{animal.occurrenceDelineations[0].separationBarriers}</p>
+                        <p>{animal.occurrenceDelineations[0].separationJustification}</p>
                     </div>
                 )
             })}
