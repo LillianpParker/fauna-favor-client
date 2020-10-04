67b4c69c688d045616ff405a2acad4eda96fd7e9
Lillian Parker <Lillianpparker@outlook.com>
2020-10-03 22:57:09 -0500

    deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Link } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
